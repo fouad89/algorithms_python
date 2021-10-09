e91683781d0e1d5781d55bfa48aaa027a0d6f0ab
Fouad <fouad89ahmed@gmail.com>
2021-10-09 08:44:55 +0100

    diagonal diff solved

--- diagonal_difference.py
+++ diagonal_difference.py
@@ -8,15 +8,9 @@ def diagonal_difference(arr):
     left_right_diag = 0
     right_left_diag = 0
     for i in range(len(arr)):
-        for j in range(len(arr)):
-            # left to right digonal 
-            if i == j:
-                left_right_diag += arr[i][j]
-            elif (i + j == len(arr)):
-                right_left_diag += arr[i][j]
-            else: pass
-    print(left_right_diag)
-    print(right_left_diag)
+        left_right_diag += arr[i][i]
+        right_left_diag += arr[i][-i-1]
+
     res = abs(left_right_diag - right_left_diag)
 
 
@@ -29,7 +23,6 @@ def diagonal_difference(arr):
 
 if __name__=='__main__':
     test_arr = [list(range(1,4)), list(range(4,7)), [9, 8, 9]]
-    print(len(test_arr))
     test2 = [[11, 2, 4], [4, 5, 6], [10, 8, -12]]
     diagonal_difference(test_arr)
     print(diagonal_difference(test_arr))
