0563b8571f40657b349ba5da3f705e5fb2d4e1d9
Fouad <fouad89ahmed@gmail.com>
2021-10-11 15:35:00 +0100

    between two set. algo design

---
+++ 09-between_two_sets.py
@@ -0,0 +1,25 @@
+# --------------------------------------------------------------------
+# There will be two arrays of integers. Determine all integers that satisfy the following two conditions:
+
+# 1. The elements of the first array are all factors of the integer being considered
+# 2. The integer being considered is a factor of all elements of the second array
+# These numbers are referred to as being between the two arrays. Determine how many such numbers exist.
+
+
+# Function Description
+
+# Complete the getTotalX function in the editor below. It should return the number of integers that are betwen the sets.
+
+# getTotalX has the following parameter(s):
+
+# int a[n]: an array of integers
+# int b[m]: an array of integers
+
+# Design:
+# Input
+## a[n], b[m] two int arrays with n & m sizes respectively
+
+# output
+## number of integers that satisfy both condtions (elems of a all divisible by number, and all elems of b are divisible by number)
+
+# ---------------------------------------------------------------------
