e21bb49341c546f568ceeba1d4d814ce01125157
Fouad <fouad89ahmed@gmail.com>
2021-10-28 10:33:36 +0100

    individual class for GA

---
+++ ga_monkey/Individual.py
@@ -0,0 +1,25 @@
+# Genetic algorithm: Monkey typing a phrase
+import random
+class Individual:
+    def __init__(self, num):
+        self.fitness = -1
+        self.genes = []
+        self.genSize = num
+        for i in range(0, num):
+            self.genes.append(chr(random.randint(32, 128)))
+
+
+    def getPhrase(self):
+        return "".join(str(x) for x in self.genes)
+
+    def getFitness(self):
+        return self.fitness
+
+    def computeFitness(self):
+        score = 0.0
+        for i in range(0, len(self.genes)):
+            if self.genes[i] == target[i]:
+                score += 1
+
+            self.fitness = score / len(target)
+                
